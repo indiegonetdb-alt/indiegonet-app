b4cefee608a6fdda8852d6947f24f4c93810acb1
indiegonetdb-alt <indiegonet1105@gmail.com>
2025-10-04 10:01:40 +0700

    fix API_URL to backend indiegonet.vercel.app

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // config.js
 export const API_URL =
-  import.meta.env.VITE_API_URL || "https://nama-backend.vercel.app/api";
+  import.meta.env.VITE_API_URL || "https://indiegonet.vercel.app/api";
